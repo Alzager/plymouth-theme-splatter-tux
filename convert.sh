29d9d1ccac2a6fb6ae30d8952a45ff08fb27dcf0
gapophustu <>
2018-04-21 22:24:22 +0200

    fixed shutdown animation

--- convert.sh
+++ convert.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-k=0
+what=$(ls -A | grep -E ".xcf$" | cut -f2 -d "-" | cut -f1 -d ".")
 
-if [[ ! -f splatter-tux/shutdown_5_0.png ]]
+if [[ ! -f splatter-$what/shutdown_5_0.png ]]
 then
-    cp -a splatter-tux.png splatter-tux/shutdown_5_0.png
+    cp -a splatter-$what.png splatter-$what/shutdown_5_0.png
 fi
 
-convert splatter-tux.png -alpha extract mask.png
+convert splatter-$what.png -alpha extract mask.png
 for j in $(ls -A | grep "shutdown_" | sort -g)
 do
-    output=$(echo splatter-tux/$(echo shutdown_5_$(echo $j | cut -c10-)))
+    output=$(echo splatter-$what/$(echo shutdown_5_$(echo $j | cut -c10-)))
     if [[ ! -f $output ]]
     then
         convert mask.png $j -composite temp.png
-        convert splatter-tux.png temp.png -alpha Off -compose CopyOpacity -composite $output
+        convert splatter-$what.png temp.png -alpha Off -compose CopyOpacity -composite $output
     fi
 done
 rm mask.png temp.png
 
+k=0
 for i in $(cat sizes)
 do
     k=$(expr $k + 1)
-    for j in $(ls -A splatter-tux/ | grep "splatter_5" | sort -g)
+    for j in $(ls -A splatter-$what/ | grep "splatter_5" | sort -g)
     do
-        output=$(echo splatter-tux/$(echo splatter_$(echo $k)_$(echo $j | cut -c12-)))
+        output=$(echo splatter-$what/$(echo splatter_$(echo $k)_$(echo $j | cut -c12-)))
         if [[ ! -f $output ]]
         then
-            convert splatter-tux/$j -resize $i $output
+            convert splatter-$what/$j -resize $i $output
         fi
     done
-    for j in $(ls -A splatter-tux/ | grep "shutdown_5" | sort -g)
+    for j in $(ls -A splatter-$what/ | grep "shutdown_5" | sort -g)
     do
-        output=$(echo splatter-tux/$(echo shutdown_$(echo $k)_$(echo $j | cut -c12-)))
+        output=$(echo splatter-$what/$(echo shutdown_$(echo $k)_$(echo $j | cut -c12-)))
         if [[ ! -f $output ]]
         then
-            convert splatter-tux/$j -resize $i $output
+            convert splatter-$what/$j -resize $i $output
         fi
     done
 done
